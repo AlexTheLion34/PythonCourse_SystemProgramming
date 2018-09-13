37defc08f70dc90410ba504aec3ff92b424be5cc
AlexTheLion <alx_pet@mail.ru>
2018-09-13 22:15:48 +0300

    Added comments

--- Task_1/FacetedSearch.py
+++ Task_1/FacetedSearch.py
@@ -1,14 +1,18 @@
 class FacetedSearch:
 
+    # Список всех птиц
     __birdsList = ["Синица", "Голубь", "Воробей", "Сорока",
                    "Дятел", "Иволга", "Кукушка", "Пеночка",
                    "Орел", "Сокол", "Сапсан", "Сплюшка",
                    "Курица", "Павлин", "Пингвин", "Страус", ]
+    # Список основных категорий
 
     __categoriesList = ["Городские", "Лесные", "Хищные", "Нелетающие"]
 
+    # Хранение выбранной пользователем категории
     __selected_category = ""
 
+    # Вывод списка птиц по категориям
     def __print_birds(self):
         count = 0
         for bird in self.__birdsList:
@@ -16,18 +20,21 @@ class FacetedSearch:
                 print("\n" + self.__categoriesList[count // 4] + ":")
             print(bird)
             count += 1
+        print()
 
+    # Определение категории
     def start_search(self):
         print("\n" + "Загадайте одну из птиц из предложенного списка: ")
         self.__print_birds()
         for category in self.__categoriesList:
-            print("\n" + "Ваша птица относится к категории - " + category.lower())
+            print("Ваша птица относится к категории - " + category.lower())
             answer = input()
             if answer == "да":
                 self.__selected_category = category
                 break
         self.__perform_search(self.__selected_category)
 
+    # Выбор конкретной категории и поиск по ней
     def __perform_search(self, category):
         if category == self.__categoriesList[0]:
             self.__search_in_first()
@@ -38,8 +45,10 @@ class FacetedSearch:
         else:
             self.__search_in_fourth()
 
+    # Функции для поиска
+    # по конкретной группе птиц
     def __search_in_first(self):
-        print("Начинается на букву 'C' ?")
+        print("Начинается на букву 'C'?")
         answer = input()
         if answer == "да":
             print("Она маленькая?")
@@ -57,17 +66,17 @@ class FacetedSearch:
                 print("Ваша птица - " + self.__birdsList[1])
 
     def __search_in_second(self):
-        print("Начинается на гласную букву ?")
+        print("Начинается на гласную букву?")
         answer = input()
         if answer == "да":
             print("Ваша птица - " + self.__birdsList[5])
         else:
-            print("Заканчивается на гласную букву ?")
+            print("Заканчивается на гласную букву?")
             answer = input()
             if answer == "да":
                 print("Ваша птица - " + self.__birdsList[4])
             else:
-                print("Ваша птица издает звук 'ку-ку' ?")
+                print("Ваша птица издает звук 'ку-ку'?")
                 answer = input()
                 if answer == "да":
                     print("Ваша птица - " + self.__birdsList[6])
@@ -75,17 +84,17 @@ class FacetedSearch:
                     print("Ваша птица - " + self.__birdsList[7])
 
     def __search_in_third(self):
-        print("Начинается на букву 'C' ?")
+        print("Начинается на букву 'C'?")
         answer = input()
         if answer == "нет":
             print("Ваша птица - " + self.__birdsList[8])
         else:
-            print("Заканчивается на гласную букву ?")
+            print("Заканчивается на гласную букву ")
             answer = input()
             if answer == "да":
                 print("Ваша птица - " + self.__birdsList[11])
             else:
-                print("Существует поезд с таким названием ?")
+                print("Существует поезд с таким названием?")
                 answer = input()
                 if answer == "да":
                     print("Ваша птица - " + self.__birdsList[10])
@@ -93,17 +102,17 @@ class FacetedSearch:
                     print("Ваша птица - " + self.__birdsList[9])
 
     def __search_in_fourth(self):
-        print("Начинается на букву 'П' ?")
+        print("Начинается на букву 'П'?")
         answer = input()
         if answer == "нет":
-            print("Большого размера ?")
+            print("Большого размера?")
             answer = input()
             if answer == "да":
                 print("Ваша птица - " + self.__birdsList[15])
             else:
                 print("Ваша птица - " + self.__birdsList[12])
         else:
-            print("Есть большой хвост ?")
+            print("Есть большой хвост?")
             answer = input()
             if answer == "да":
                 print("Ваша птица - " + self.__birdsList[13])
